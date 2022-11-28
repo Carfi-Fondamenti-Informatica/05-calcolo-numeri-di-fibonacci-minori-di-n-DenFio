62da76c780524dd10a7de64fe74ed764936ddfa3
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-11-28 08:35:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     int n=0, a=1, b=1, c=0;
     cin >> n;
         cout << a << endl << b << endl;
-        for (int i=0; 1<=n; i++) {
+        for (int i=0; i<=n; i++) {
            c=a+b;
            a=b;
            b=c;
