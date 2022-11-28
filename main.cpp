01522d1de6c051bebe1ee8fe6f1be45d548787b7
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-11-28 08:16:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+    int n=0, a=1, b=1, c=0;
+    cin >> n;
+        cout << a << endl << b << endl;
+        for (int i=0; 1<=n; i++) {
+           c=a+b;;
+           a=b;
+           b=c;
+           cout << c << endl;
+ }   
    return 0;
 }
